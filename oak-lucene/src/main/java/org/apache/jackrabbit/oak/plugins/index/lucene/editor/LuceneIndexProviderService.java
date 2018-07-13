229afb3affc307d809d300e249065f5cfa9b0ef2
Thomas Mueller <mueller@adobe.com>
2018-07-13 15:34:10 +0200

    OAK-7629: Lucene index: hide the settings COR and COW

--- oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/editor/LuceneIndexProviderService.java
+++ oak-lucene/src/main/java/org/apache/jackrabbit/oak/plugins/index/lucene/editor/LuceneIndexProviderService.java
@@ -149,7 +149,8 @@ public class LuceneIndexProviderService {
     @Property(
             boolValue = true,
             label = "Enable CopyOnRead",
-            description = "Enable copying of Lucene index to local file system to improve query performance"
+            description = "Enable copying of Lucene index to local file system to improve query performance",
+            propertyPrivate = true
     )
     private static final String PROP_COPY_ON_READ = "enableCopyOnReadSupport";
 
@@ -165,7 +166,8 @@ public class LuceneIndexProviderService {
     @Property(
             boolValue = PROP_COPY_ON_WRITE_DEFAULT,
             label = "Enable CopyOnWrite",
-            description = "Enable copying of Lucene index to local file system to improve index writer performance"
+            description = "Enable copying of Lucene index to local file system to improve index writer performance",
+            propertyPrivate = true
     )
     private static final String PROP_COPY_ON_WRITE = "enableCopyOnWriteSupport";
 
